14d38a4d21cf45c38d9d807e2d15db5f20db3dd3
Giuseppe Tipaldi <ing.giuseppe.tipaldi@gmail.com>
2020-04-10 13:55:11 +0200

    ihb-fw: improve documentation

--- fw/ihb.h
+++ fw/ihb.h
@@ -2,7 +2,7 @@
 #define IHB_H
 
 #include <stdio.h>
-#include <stdbool.h> 
+#include <stdbool.h>
 
 #ifdef MODULE_IHBNETSIM
 /* SK_N_S: Skin nodes for IHB */
@@ -12,21 +12,24 @@
 #define SK_T_S (12u)
 
 /**
- * @struct skin_node
+ * struct skin_node - represent the collected data that is incoming by skin nodes
+ *
+ * The Struct is filled by ihb-netsim driver
  *
  * @data: payload, the output of the tactile sensors
- * @address: node address
- * @expired: each time that IHB acquires payload assume set it true, and
- *           it's set ony by skin node
+ * @address: node's address
+ * @expired: Each time that the IHB acquires payload from SKIN node, that means
+ *           that the node is still alive so expired is not set.
+ *           Netsim set is always false.
  *
- * Struct size 114bytes.
+ * Struct size dipends by SK_T_S, if SK_T_S == 12, struct is 16bytes.
  */
 struct skin_node {
 	uint8_t data[SK_T_S];
 	uint16_t address;
 	bool expired;
 };
-#define SK_THREAD_HELP	"skin simulator thread"
+#define SK_THREAD_HELP	"ihb-skin net simulator"
 #endif
 
 #ifdef MODULE_IHBCAN
@@ -42,11 +45,11 @@ struct skin_node {
 #define CAN_C (0)
 
 /**
- * @struct ihb_can_perph
+ * struct skin_node - contains all data that are used by ihb-can driver
  *
- * @controller_uid: MCU unique ID
- * @name: it contains the name of can controller
- * @id: identifier number of can the controller of the mcu
+ * @controller_uid: MCU's unique ID
+ * @name: the name of the can controller
+ * @id: identifier number of the MCU's can controller
  * @frame_id: CAN frame ID, it's obtainded by the MCU unique ID
  * @status_notify_node: if true the IHB node is announcing itself on CAN bus
  * @master: true when the IHB node is master, false otherwise.
@@ -60,9 +63,14 @@ struct ihb_can_perph {
 	bool master;
 };
 
-#define IHB_THREAD_HELP "ihb - can submodule, development branch"
+#define IHB_THREAD_HELP "ihb-can driver"
 #endif
 
+/**
+ * struct ihb_structs - it's a containter of pointers
+ *
+ * The modules must be independent on one each other
+ */
 struct ihb_structs {
 #ifdef MODULE_IHBCAN
 	struct ihb_can_perph **can;
