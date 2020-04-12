6fb6095e5ac7fcee05eb14bd52ab42244741e6db
Giuseppe Tipaldi <ing.giuseppe.tipaldi@gmail.com>
2020-04-12 17:28:46 +0200

    ihb-tool: rework of the logic

--- host/frontend/main.c
+++ host/frontend/main.c
@@ -28,30 +28,28 @@ void _signals(const int signo)
 {
 	switch (signo) {
 		case SIGTERM:
-			fprintf(stderr, "received signal %i\n", signo);
+			fprintf(stderr, "[*] received signal %i\n", signo);
 			running = false;
 			break;
 		case SIGHUP:
-          		fprintf(stderr, "received signal %i\n", signo);
+          		fprintf(stderr, "[*] received signal %i\n", signo);
 			running = false;
 			break;
 		case SIGINT:
-          		fprintf(stderr, "received signal %i\n", signo);
+          		fprintf(stderr, "[*] received signal %i\n", signo);
 			running = false;
 			break;
 		default:
            		break;
 	}
-
 }
 
 int main(int argc, char **argv)
 {
+	bool verbose = false, isotp_fails = false;
+	int can_soc_raw = 0, can_soc_isotp = 0;
 	uint8_t master_id = 255, ihb_nodes = 0;
-	bool verbose = false;
 	char *perph = NULL;
-	int can_soc_raw = 0;
-	int can_soc_isotp = 0;
 	int c, r = 0;
 	void *data;
 
@@ -82,11 +80,11 @@ int main(int argc, char **argv)
 
 	/* validate input */
 	if (!perph) {
-		fprintf(stderr, "Please specify a CAN device\n");
+		fprintf(stderr, "[!] Please specify a CAN device\n");
                 help(argv[0]);
 		return EXIT_FAILURE;
 	} else if (strlen(perph) > IFNAMSIZ) {
-		fprintf(stderr, "name of CAN device '%s' is too long!\n", perph);
+		fprintf(stderr, "[!] name of CAN device '%s' is too long!\n", perph);
 		help(argv[0]);
 		return EXIT_FAILURE;
 	}
@@ -94,75 +92,115 @@ int main(int argc, char **argv)
 	/* Open and inizializate the socket CAN */
 	r = ihb_init_socket_can(&can_soc_raw, perph);
 	if (r < 0)
-		goto _fail;
+		goto _fail0;
 
 	/* Start discovery of the nodes */
 	r = ihb_discovery(can_soc_raw, verbose, &master_id, &ihb_nodes);
 	if (r < 0)
-		goto _free_list;
+		goto _fail1;
 
 	if(ihb_nodes != 0) {
-		fprintf(stdout, "Network size %d. IHB master candidate = %02x\n",
+		fprintf(stdout, "[*] Network size %d. IHB master candidate = %02x\n",
 				ihb_nodes, master_id);
 	} else {
-		puts("IHBs not found");
-		goto _fail;
+		fprintf(stderr, "[!] There are not IHBs available");
+		goto _fail1;
 	}
 
 	/* Start the setup of the nodes */
 	r = ihb_setup(can_soc_raw, master_id, verbose);
 	if (r < 0)
-		goto  _free_list;
+		goto  _fail1;
 
-	 /* Open and inizializate the socket CAN ISO-TP */
-	r = ihb_init_socket_can_isotp(&can_soc_isotp, perph, master_id);
-	if (r < 0)
-		 goto  _free_list;
+	while (running) {
 
-	while (running && ihb_nodes > 0) {
-		r = ihb_rcv_data(can_soc_isotp, &data, verbose);
+		/*
+		 * To start, the ISO-TP needs a valid IHB node. This logic
+		 * assumes as valid only the IHB node with an 0 < ID < 256
+		 *
+		 * The IHB's fw, implements a fletcher8 (unit8_t) function that
+		 * obtains an ID from the MCU's unique ID.
+		 *
+		 * The next code path is executed when the ihbtool has discovered
+		 * the IHBs nodes. The ihb_nodes counts the IHB.
+		 */
+		if(ihb_nodes > 0) {
 
-		/* If the IHB goes in timeout, it has passed away */
-		if(r == -ETIMEDOUT) {
-			r = ihb_blacklist_node(master_id, verbose);
+			/* Open and inizializate the socket CAN ISO-TP */
+			r = ihb_init_socket_can_isotp(&can_soc_isotp, perph, master_id);
 			if (r < 0) {
-				printf(stderr, "Cannot blacklist the IHB node=%#x\n", master_id);
 				break;
 			}
-			// !TODO Add logic
-			//  1. to search new candidate
-			//  2. to configure new candidate
-		} else {
-			/* The Fault mechanism covers only ETIMEDOUT */
-			break;
+
+			/* Stat the IHB data acquisition */
+			r = ihb_rcv_data(can_soc_isotp, &data, verbose);
+
+			/*
+			 * If the IHB goes in timeout, it has passed away...
+			 * The anti fault mechanism covers only ETIMEDOUT
+			 */
+			if(r == -ETIMEDOUT) {
+
+				/* Close old socket */
+				shutdown(can_soc_isotp, 2);
+				close(can_soc_isotp);
+
+				isotp_fails = true;
+				ihb_nodes--;
+
+				/* If it was the last exit */
+				if(ihb_nodes == 0) {
+					fprintf(stdout, "[*] There are not IHBs available\n");
+					break;
+				}
+
+			} else {
+				fprintf(stderr, "[!] IHB stops to send data, err=%d\n", r);
+				/* Stop if any unknown errors happen */
+				break;
+			}
 		}
 
-		ihb_nodes--;
-		fprintf(stdout, "Network size %d. IHB master candidate = %02x\n",
-				ihb_nodes, master_id);
-	
-		/* Close old socket */
-		shutdown(can_soc_isotp, 2);
-		close(can_soc_isotp);
+		if(isotp_fails) {
 
-		 /* Open and inizializate the socket CAN ISO-TP */
-		r = ihb_init_socket_can_isotp(&can_soc_isotp, perph, master_id);
+			r = ihb_blacklist_node(master_id, verbose);
+			if (r < 0) {
+				fprintf(stderr, "[!] Cannot blacklist the IHB node=%#x\n", master_id);
+				/* Stop if any unknown errors happen */
+				break;
+			}
+
+			/* Reset the IHB master candidate */
+			master_id = 255;
+
+			/* Search for another IHB candidate  */
+			r = ihb_discovery_newone(&master_id, verbose);
+			if (r < 0)
+				break;
+		}
+
+		isotp_fails = false;
+		fprintf(stdout, "[*] Network size %d. IHB master candidate = %02x\n",
+				ihb_nodes, master_id);
+
+		/* Start the setup of the nodes */
+		r = ihb_setup(can_soc_raw, master_id, verbose);
 		if (r < 0)
-			 running = false;
+			break;
 	}
 
-	HASH_CLEAR(hh,ihbs);
+
+	free(data);
 
 	shutdown(can_soc_isotp, 2);
 	close(can_soc_isotp);
 
+_fail1:
 	shutdown(can_soc_raw, 2);
 	close(can_soc_raw);
 
-_free_list:
 	HASH_CLEAR(hh,ihbs);
 
-_fail:
-
+_fail0:
 	return r;
 }
