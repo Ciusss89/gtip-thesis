bf5f567fc33169d120a7095ac7fdb9353282e93a
Giuseppe Tipaldi <ing.giuseppe.tipaldi@gmail.com>
2020-04-13 12:01:57 +0200

    tools: env_simulator: prepare to support real CAN device

--- host/env_simulator.sh
+++ host/env_simulator.sh
@@ -2,6 +2,7 @@
 #
 
 IHB_MAGIC="5F49484205F5565F"
+DEVICE="vcan0"
 
 _init_periph() {
 	modprobe vcan || {
@@ -9,29 +10,34 @@ _init_periph() {
 		exit
 	}
 
-	ip link add dev vcan0 type vcan && \
-			ip link set up vcan0
+	ip link add dev $DEVICE type vcan
+}
+
+_periph_up() {
+	ip link set up $DEVICE
+}
+
+_kill() {
+	kill $PID_IHB8 $PID_IHB20 $PID_IHB101 $PID_IHB121 $PID_IHB243 $PID_IHB4
 }
 
 _clean() {
-	kill $PID_IHB8 $PID_IHB20 $PID_IHB101 $PID_IHB121 $PID_IHB243 $PID_IHB4
-
 	ip link delete dev vcan0
 
 	modprobe -r vcan
 }
 
 _simulate_notify() {
-	cangen vcan0 -I 8 -g 2500 -D $IHB_MAGIC -L 8 & PID_IHB8="$!"
-	cangen vcan0 -I 20 -g 1500 -D $IHB_MAGIC -L 8 & PID_IHB20="$!"
-	cangen vcan0 -I 65 -g 4000 -D $IHB_MAGIC -L 8 & PID_IHB101="$!"
-	cangen vcan0 -I 121 -g 5700 - & PID_IHB121="$!"
-	cangen vcan0 -I 234 -g 5900 & PID_IHB243="$!"
-	cangen vcan0 -I 4 -g 2000 -L 8 & PID_IHB4="$!"
+	cangen $DEVICE -I 8 -g 2500 -D $IHB_MAGIC -L 8 & PID_IHB8="$!"
+	cangen $DEVICE -I 20 -g 1500 -D $IHB_MAGIC -L 8 & PID_IHB20="$!"
+	cangen $DEVICE -I 65 -g 4000 -D $IHB_MAGIC -L 8 & PID_IHB101="$!"
+	cangen $DEVICE -I 121 -g 5700 - & PID_IHB121="$!"
+	cangen $DEVICE -I 234 -g 5900 & PID_IHB243="$!"
+	cangen $DEVICE -I 4 -g 2000 -L 8 & PID_IHB4="$!"
 }
 
 _dump_can() {
-	gnome-terminal -q -- candump vcan0 -c -a
+	gnome-terminal -q -- candump $DEVICE -c -a
 }
 
 if [ "$EUID" -ne 0 ]
@@ -41,10 +47,12 @@ fi
 
 
 _init_periph
+_periph_up
 
 _simulate_notify
 
 _dump_can
 echo "enter to exit" && read;
 
+_kill
 _clean
