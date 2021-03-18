62ba9dba53f4543009ca96ff31a461b63d04b27b
Giuseppe Tipaldi <ing.giuseppe.tipaldi@gmail.com>
2021-03-18 20:38:37 +0100

    script: improve nastive setup

--- script/setup-native.sh
+++ script/setup-native.sh
@@ -4,5 +4,7 @@
 sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan
 sudo ip link add dev vcan1 type vcan
+sudo ip link set vcan0 type can bitrate 1000000
+sudo ip link set vcan1 type can bitrate 1000000
 sudo ip link set vcan0 up
 sudo ip link set vcan1 up
